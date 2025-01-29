c8c2c6a01e68af1ff5d1f1554154759c5b0e3698
Francisco <francisco@mondey.co>
2025-01-28 23:05:13 -0300

    PostHog will only work in production environment

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,7 @@
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 
-if (typeof window !== 'undefined') {
+if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
         api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
         person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
@@ -12,6 +12,10 @@ if (typeof window !== 'undefined') {
 export default function CSPostHogProvider({ children }: Readonly<{
     children: React.ReactNode;
 }>) {
+    if (process.env.NODE_ENV !== 'production') {
+        return <>{children}</>
+    }
+
     return (
         <PostHogProvider client={posthog} >
             {children}
